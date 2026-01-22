c8e17aaa0020cf3f87591b3bc776c4f8b9f24958
MaryLangdon <LWZarathustra@proton.me>
2026-01-22 14:59:57 +0800

    Add files via upload

--- generate_index.py
+++ generate_index.py
@@ -1,42 +1,40 @@
-import os
-import time
-import re
+import os, time, re
 
-def get_preview(path):
+def get_clean_text(path):
     try:
         with open(path, 'r', encoding='utf-8') as f:
-            content = f.read()
-        text = re.sub(r'<(style|script)[^>]*>.*?</\1>', '', content, flags=re.DOTALL)
-        text = re.sub(r'<[^>]+>', '', text)
-        return " ".join(text.split())[:100] + "..."
+            c = f.read()
+        # 彻底移除样式和脚本，防止预览里出现代码
+        c = re.sub(r'<(style|script)[^>]*>.*?</\1>', '', c, flags=re.DOTALL)
+        text = re.sub(r'<[^>]+>', '', c)
+        return " ".join(text.split())[:120] + "..."
     except:
         return ""
 
-def main():
+def build():
     posts_dir = 'posts'
-    if not os.path.exists(posts_dir):
-        os.makedirs(posts_dir)
+    if not os.path.exists(posts_dir): os.makedirs(posts_dir)
     
     files = [f for f in os.listdir(posts_dir) if f.endswith('.html')]
     files.sort(key=lambda x: os.path.getmtime(os.path.join(posts_dir, x)), reverse=True)
 
     rows = ""
     for f in files:
-        full_path = os.path.join(posts_dir, f)
-        m_time = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(full_path)))
-        preview = get_preview(full_path)
+        f_path = os.path.join(posts_dir, f)
+        date = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(f_path)))
+        preview = get_clean_text(f_path)
         rows += f'''
         <a href="{posts_dir}/{f}" class="article-row">
-            <div class="cell name">{f}</div>
-            <div class="cell date">{m_time}</div>
-            <div class="cell preview-text">{preview}</div>
+            <div class="cell">{f}</div>
+            <div class="cell">{date}</div>
+            <div class="cell preview">{preview}</div>
         </a>'''
 
     with open('template.html', 'r', encoding='utf-8') as t:
-        tpl = t.read()
+        html = t.read().replace('', rows)
     
     with open('index.html', 'w', encoding='utf-8') as i:
-        i.write(tpl.replace('', rows))
+        i.write(html)
 
 if __name__ == "__main__":
-    main()
+    build()
