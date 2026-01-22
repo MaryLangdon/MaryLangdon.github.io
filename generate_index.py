ccc6444e4c4e2132ee7852b71d741e0b48ae8720
MaryLangdon <LWZarathustra@proton.me>
2026-01-22 15:04:08 +0800

    Add files via upload

--- generate_index.py
+++ generate_index.py
@@ -1,10 +1,9 @@
 import os, time, re
 
-def get_clean_text(path):
+def get_text(path):
     try:
         with open(path, 'r', encoding='utf-8') as f:
             c = f.read()
-        # 彻底移除样式和脚本，防止预览里出现代码
         c = re.sub(r'<(style|script)[^>]*>.*?</\1>', '', c, flags=re.DOTALL)
         text = re.sub(r'<[^>]+>', '', c)
         return " ".join(text.split())[:120] + "..."
@@ -20,14 +19,14 @@ def build():
 
     rows = ""
     for f in files:
-        f_path = os.path.join(posts_dir, f)
-        date = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(f_path)))
-        preview = get_clean_text(f_path)
+        f_p = os.path.join(posts_dir, f)
+        dt = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(f_p)))
+        pv = get_text(f_p)
         rows += f'''
         <a href="{posts_dir}/{f}" class="article-row">
             <div class="cell">{f}</div>
-            <div class="cell">{date}</div>
-            <div class="cell preview">{preview}</div>
+            <div class="cell">{dt}</div>
+            <div class="cell preview">{pv}</div>
         </a>'''
 
     with open('template.html', 'r', encoding='utf-8') as t:
